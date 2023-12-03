85c3af1f51e0380de2d6c11e93e3189910ff2069
BielssNabil <satyanabil461@gmail.com>
2023-12-03 13:32:51 +0700

    feat: add text under maintenance

--- src/pages/components/ProjectCom.tsx
+++ src/pages/components/ProjectCom.tsx
@@ -6,6 +6,7 @@ const ProjectCom = () => {
       <h1 className="text-4xl font-bold text-center py-20">Project</h1>
       <div className="min-h-full flex items-center justify-center">
         <h1 className="text-6xl text-center">COMING SOON</h1>
+        <h2 className="text-xl">Under Maintenance</h2>
       </div>
     </div>
   );
